51e5663d7e46c8cf921bcefb34eeb68c76e2873c
Jbolts <77908382+Jbolton89@users.noreply.github.com>
2021-06-01 21:58:16 +1000

    last commit

--- Server.js
+++ Server.js
@@ -267,32 +267,6 @@ const addEmployee = async () => {
 });
 }
 
-        // const updateDepartment = () => { 
-        // // const updateProduct = () => {
-        //   console.log('Updating all Rocky Road quantities...\n');
-        //   const query = connection.query(
-        //     'UPDATE products SET ? WHERE ?',
-        //     [
-        //       {
-        //         quantity: 100,
-        //       },
-        //       {
-        //         flavor: 'Rocky Road',
-        //       },
-        //     ],
-        //     (err, res) => {
-        //       if (err) throw err;
-        //       console.log(`${res.affectedRows} products updated!\n`);
-        //       // Call deleteProduct AFTER the UPDATE completes
-        //       deleteProduct();
-        //     }
-        //   );
-
-        //   // logs the actual query being run
-        //   console.log(query.sql);
-        // };
-
-        // }
 
         const updateDepartment = () => {
             //  for each as the choices to choose from
